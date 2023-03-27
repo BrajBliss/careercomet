dc41b42334d67cd79b369a32017df10e724c6b6f
Amay <brajbliss@gmail.com>
2023-03-27 18:31:09 +0530

    home, cards, scroll, buttons

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,78 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	Image,
+	FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = [
+	'Full Time',
+	'Part Time',
+	'Freelance',
+	'Internship',
+	'Contract',
+	'Volunteer',
+];
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+	const router = useRouter();
+	const [activeJobType, setActiveJobType] = useState('Full Time');
 
-export default Welcome
+	return (
+		<View>
+			<View style={styles.container}>
+				<Text style={styles.userName}>Hello John</Text>
+				<Text style={styles.welcomeMessage}>Find your Perfect Job</Text>
+			</View>
+
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput
+						style={styles.searchInput}
+						value=''
+						onChange={() => {}}
+						placeholder='What are you looking for?'
+					/>
+				</View>
+
+				<TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+					<Image
+						source={icons.search}
+						resizeMode='contain'
+						style={styles.searchBtnImage}
+					/>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.tabsContainer}>
+				<FlatList
+					data={jobTypes}
+					renderItem={({ item }) => (
+						<TouchableOpacity
+							style={styles.tab(activeJobType, item)}
+							onPress={() => {
+								setActiveJobType(item);
+								router.push(`/search/${item}`);
+							}}>
+							<Text style={styles.tabText(activeJobType, item)}>
+								{item}
+							</Text>
+						</TouchableOpacity>
+					)}
+					keyExtractor={(item) => item}
+					contentContainerStyle={{ columnGap: SIZES.small }}
+					horizontal
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default Welcome;
